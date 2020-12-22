c4eb309dec33e921d1721344082456822b2f7cae
kouroshbehzadian <k.behzadian-moghadam@exeter.ac.uk>
2020-12-22 21:00:11 +0000

    trimmed main and getconfig

--- Main.m
+++ Main.m
@@ -1,14 +1,15 @@
 % %The main file to set the algorithms and run for CSA, PSO, and GA
-format long; close all; clear all; clc
-out = get_config('PSO');
+format long; close all; clc
+
+Out = get_config('PSO');
 
 switch Algorithm
     %%Crow Search Algorithm
     case 'CSA'
-        [Global fit_mem g_best]=CSA(Npop,Nvar,Func,Nrun,itermax,LB,UB,AP,fl);
+        [Global,fit_mem,g_best]=CSA(Out);
         
     case 'PSO'
-        [BestCost BestSolution]=PSO(Npop,Nvar,Func,Nrun,itermax,LB,UB,w,wdamp,c1,c2);
+        [BestCost,BestSolution]=PSO(Out);
         
 end
 
