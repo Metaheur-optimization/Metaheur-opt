46643614da0479cd3b73b658b1b2ae06357680df
Farshid Rahmani <59573719+FRahmani368@users.noreply.github.com>
2021-02-20 14:29:51 -0500

    minor issues with plotting

--- Main.m
+++ Main.m
@@ -1,7 +1,7 @@
 % %The main file to set the algorithms and run for CSA, PSO, and GA
 tic
 format long; close all; clc; clear; 
-
+addpath('plots')
 Algorithm='LAL'; 
 
 Out = get_config(Algorithm);
@@ -27,57 +27,10 @@ toc
 
 
 
-% disp(['No of Runs=',num2str(Out.NRun),' No of Iteration=',num2str(Out.MaxIter),...
-%     ' Population Size=',num2str(Out.Npopulation)])
-% 
-% 
-% for i=1:Out.NRun
-%     plot(AllBestFitnesses(i,1:50))
-%     xlabel('No of Iteration')
-%     ylabel('Fitness value')
-%     hold on
-% end
-
-% figure(1)
-% B=mean(Results.AllBestFitnesses);
-% subplot(3,1,1)
-% 
-% %nexttile
-% plot(B(1,1:numel(B)))
-% title(['Average Fitness value for Engineering Function number ', num2str(Out.EngFunction)])
-% 
-% 
-% subplot(3,1,2)
-% 
-% 
-% %nexttile
-% C=mean(Results.NFE);
-% plot(C(1,1:numel(B)))
-% title('Number of Function Evaluations')
-% % ylim([0 50])
-% 
-% subplot(3,1,3)
-% 
-% %nexttile
-% D=mean(Results.Feasible);
-% plot(D(1,1:numel(B)))
-% hold on
-% E=C-D;
-% plot(E(1,1:numel(B)))
-% title('Number of Feasible and Infeasible Solutions')
-% legend('Feasible Solutions','Infeasible Solutions' );
-% ylim([0 50])
-% xlabel('Iteration number')
-% hold off
-% 
-% figure(2)
-% plot3(Results.AllBestSolution(:,1),Results.AllBestSolution(:,2),Results.AllBestSolution(:,3),'bo', 'MarkerSize',10)
-% xlabel('X1')
-% ylabel('X2')
-% zlabel('X3')
-% title ('Best Solution Decion Variables')
-% grid on
-% % surf(Results.Feasible)
+figure(1)
+plotFeasible(Results, Out)
+figure(2)
+plot3Dbest(Results)
 % 
 % EE=mean(Results.AllBestFitnesses, 'all')
 % NFE=Results.NFEAll;% Number of function evluations accouting for all population size, iterations and runs
